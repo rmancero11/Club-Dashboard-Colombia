08aba46f4f20b99bb59d5dfe2702852740ce651e
Ricardo Mancero <106605161+rmancero11@users.noreply.github.com>
2025-10-23 13:53:42 -0500

    Change title quotes from double to single

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -1,5 +1,4 @@
-export const metadata = { title: "Login | Club de Viajeros Solteros" };
-
+export const metadata = { title: 'Login | Club de Viajeros Solteros' };
 export default function LoginLayout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
