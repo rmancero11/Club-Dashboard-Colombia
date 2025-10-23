f075e07243c70ad8e42d99fb6088249e11b48113
Ricardo Mancero <106605161+rmancero11@users.noreply.github.com>
2025-10-23 13:27:50 -0500

    Add LoginLayout component with metadata

---
+++ app/login/layout.tsx
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: "Login | Club de Viajeros Solteros",
+};
+
+export default function LoginLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
